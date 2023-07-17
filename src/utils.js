a58b033c7f0bc1d4d6679b35dc7a2c22adc38ca8
AlinaNesmiianova <139108165+AlinaNesmiianova@users.noreply.github.com>
2023-07-17 13:04:48 +0300

    Add files via upload

---
+++ src/utils.js
@@ -0,0 +1,9 @@
+export const inversionFilter = (imageData) =>{
+    let pixels = imageData.data
+    for(var i = 0; i < pixels.length; i+=4){
+        pixels[i] = 255 - pixels[i]
+        pixels[i+1] = 255 - pixels[i+1]
+        pixels[i+2] = 255 - pixels[i+2]
+    }
+    return imageData
+}
