4ba6b4a39da3cee32ec52455eab2d13e098f6a0f
DmitryShar0v <139542726+DmitryShar0v@users.noreply.github.com>
2023-07-17 21:08:30 +0300

    brightness + reflections

--- src/App.js
+++ src/App.js
@@ -1,36 +1,59 @@
-import './App.css';
-import Canvas from "./Canvas";
-import image from "./image.jpg";
+import './App.css'
+import Canvas from "./Canvas"
+import image from "./image.jpg"
 import image2 from "./image2.jpg"
-import{makeWB} from "./Black and white";
-import {inversionFilter, brightness} from "./utils"
+import { makeWB } from "./Black and white"
+import { inversionFilter, brightness } from "./utils"
+import { flipHorizontal, flipVertical } from "./utils"
 
 function App() {
-const processImage = (imageData)=>{
-  return makeWB(imageData)
-}
-  const processImage2 = (imageData)=>{
+  const processImage = (imageData) => {
+    return makeWB(imageData)
+  }
+  const processImage2 = (imageData) => {
     return inversionFilter(imageData)
   }
-  const processImage3 = (imageData)=>{
-    return brightness(imageData,5)
+  const processImage3 = (imageData) => {
+    return brightness(imageData, 5)
+  }
+  const processImage4 = (imageData) => {
+    return flipHorizontal(imageData)
+  }
+  const processImage5 = (imageData) => {
+    return flipVertical(imageData)
   }
   return (
     <>
-    <div className="Indent">
+      <div className="Indent">
         Черно-белый:
-    </div>
-    <div className="App">
-        <Canvas image ={image} processImage={processImage}/>
-        
-    </div>
-    <div className="Indent">
+      </div>
+      <div className="App">
+        <Canvas image={image} processImage={processImage} />
+      </div>
+      <div className="Indent">
         Инверсия:
-    </div>
-    <div className="App">
-      <Canvas image ={image2} processImage={processImage2}/>
-    
-    </div>
+      </div>
+      <div className="App">
+        <Canvas image={image2} processImage={processImage2} />
+      </div>
+      <div className="Indent">
+        Яркость:
+      </div>
+      <div className="App">
+        <Canvas image={image} processImage={processImage3} />
+      </div>
+      <div className="Indent">
+        Отражение по горизонтали:
+      </div>
+      <div className="App">
+        <Canvas image={image} processImage={processImage4} />
+      </div>
+      <div className="Indent">
+        Отражение по вертикали:
+      </div>
+      <div className="App">
+        <Canvas image={image} processImage={processImage5} />
+      </div>
     </>
   );
 }
