f8a77b92ee0b0941e4b61a8949c5e261d6010fbe
Alinasukh <139049005+Alinasukh@users.noreply.github.com>
2023-07-17 12:33:51 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Canvas from "./Canvas";
 import image from "./image.jpg";
-import{makeWB} from "./BW";
+import{makeWB} from "./Black and white";
 
 function App() {
 const processImage = (imageData)=>{
