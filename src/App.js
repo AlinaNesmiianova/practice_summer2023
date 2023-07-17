4160a7ac3fb3f686568595d3d490a1ca2de3c540
Alina N <nesmeyanova.alina@inbox.ru>
2023-07-17 12:15:01 +0300

    Создала utils.js и image2.jpg,  добавила inversionFilter() в utils.js и сделала соответствующие правки для работы с этим фильтром в App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import Canvas from "./Canvas";
 import image from "./image.jpg";
+import image2 from "./image2.jpg"
 import{makeWB} from "./BW";
+import {inversionFilter} from "./utils"
 
 function App() {
 const processImage = (imageData)=>{
   return makeWB(imageData)
 }
+  const processImage2 = (imageData)=>{
+    return inversionFilter(imageData)
+  } 
+
   return (
     <div className="App">
         <Canvas image ={image} processImage={processImage}/>
+        <Canvas image ={image2} processImage={processImage2}/>
     </div>
   );
 }
