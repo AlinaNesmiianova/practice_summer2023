9fb23d76b19b3e34ca238ae473ed8367901fe853
AlinaNesmiianova <139108165+AlinaNesmiianova@users.noreply.github.com>
2023-07-17 13:06:21 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import Canvas from "./Canvas";
 import image from "./image.jpg";
+import image2 from "./image2.jpg"
 import{makeWB} from "./Black and white";
+import {inversionFilter} from "./utils"
 
 function App() {
 const processImage = (imageData)=>{
   return makeWB(imageData)
 }
+  const processImage2 = (imageData)=>{
+    return inversionFilter(imageData)
+  } 
   return (
     <div className="App">
         <Canvas image ={image} processImage={processImage}/>
+        <Canvas image ={image2} processImage={processImage2}/>
     </div>
   );
 }
